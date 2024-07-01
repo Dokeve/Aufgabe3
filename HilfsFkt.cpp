4a094ee056dd8e41cd199d6539a402fc56da6fef
Derek Aghoro <D.Aghoro@web.de>
2024-07-01 19:06:26 +0200

    io Hilfsfunktionen in Hifsdatei implementiert

--- HilfsFkt.cpp
+++ HilfsFkt.cpp
@@ -1,15 +1,33 @@
 #include "HilfsFkt.h"
 #include<iostream>
-
+using namespace std;
 rectmat operator*(const double scal, const rectmat mat1){
 	return rectmat();
 }
 
-std::ostream& operator<<(std::ostream& os, const rectmat& mat1){
+ostream& operator<<(ostream& os, const rectmat& mat1){
+
+	for (int i = 0; i < mat1.getRow(); i++) {
+		for (int j = 0; j < mat1.getCol(); j++) {
+			os << mat1.get(i,j)<< "\t";
+		}
+		os << "\n";
+	}
+	os << endl;
 	return os;
 }
 
 std::istream& operator>>(std::istream& in, rectmat& mat1){
+	double s = 0;
+	cout << "Matrixwerte Zeilenweise eingeben" << endl;
+	for (int i = 0; i < mat1.getRow(); i++) {
+		for (int j = 0; j < mat1.getCol(); j++) {
+			cout << "Matrixwerte Elementweise eingeben:";
+			in >> s;
+			mat1.set(i, j, s);
+		}
+	}
+	cout << endl;
 	return in;
 }
 
